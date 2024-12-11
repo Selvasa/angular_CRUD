b35e8572cd6d04f699c120f205acbd8d9f2b49fd
Darwin Sakthivel <darwin.sakthivel@knacksystems.com>
2024-12-11 17:16:30 +0530

    done crud

--- src/comp/crud.service.ts
+++ src/comp/crud.service.ts
@@ -3,28 +3,32 @@ import { IStudent } from '../model/student';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CrudService {
-  private http = inject(HttpClient);
 
-  constructor() { }
-  postStudentData(studentData: IStudent) {
-    return this.http.post('http://localhost:3000/students', studentData)
-  }
+    private http = inject(HttpClient);
+    ajith = 10;
+    constructor() { }
+    postStudentData(studentData: IStudent) {
+        return this.http.post('http://localhost:3000/students', studentData)
+    }
 
-  getStudentData() {
-    return this.http.get('http://localhost:3000/students')
-  }
+    getStudentData() {
+        return this.http.get('http://localhost:3000/students')
+    }
 
-  deleteStudent(id: any) {
-    return this.http.delete('http://localhost:3000/students/' + id)
-  }
+    deleteStudent(id: any) {
+        return this.http.delete('http://localhost:3000/students/' + id)
+    }
 
-  getSingleStudent(id: any) {
-    return this.http.get('http://localhost:3000/students/' + id)
-  }
-  updateOne(id: any, singlestudent: any) {
-    return this.http.put('http://localhost:3000/students/' + id, singlestudent)
-  }
+    getSingleStudent(id: any) {
+        return this.http.get('http://localhost:3000/students/' + id)
+    }
+    updateOne(id: any, singlestudent: any) {
+        return this.http.put('http://localhost:3000/students/' + id, singlestudent)
+    }
+    getEmpDetails() {
+        return this.http.get('http://localhost:3000/emp');
+    }
 }
--- src/comp/list/list.component.ts
+++ src/comp/list/list.component.ts
@@ -1,42 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, EventEmitter, inject, input, Input, Output, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, inject, input, Input, Output, signal, ViewChild, ViewContainerRef } from '@angular/core';
 import { CrudService } from '../crud.service';
+import { CustDirDirective } from '../../directive/cust-dir.directive';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CustDirDirective],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  service = inject(CrudService);
-  cdr = inject(ChangeDetectorRef);
 
-  @Input()studentList:any;
-  @Output() singleStudent = new EventEmitter();
-
-  // studentList = signal<any>([]);
-
-
-  constructor() { }
-
-  ngOnChanges() {
-    // this.service.getStudentData().subscribe((res: any) => {
-      console.log(this.studentList);
-
-    //   this.studentList.set(res);
-    // })
-  }
-
-
-  deleteData(id: any) {
-    this.service.deleteStudent(id).subscribe()
-  }
+  @Input() studentList: any[] = [];
+  @Output() sStudent = new EventEmitter<any>();
+  @Output() sDelete = new EventEmitter<any>();
 
   getSingle(id: any) {
-    this.service.getSingleStudent(id).subscribe((res) => {
-      this.singleStudent.emit(res)
-    })
+    this.sStudent.emit(id);
+  }
+  deleteData(id: any) {
+    console.log(id);
+    this.sDelete.emit(id)
   }
 }
